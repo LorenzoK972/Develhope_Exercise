b258c9e7cdfa276a1bf7cd9640eeccafff64823a
Lorenzo <cap.lorenzo@yahoo.it>
2023-03-18 13:29:00 +0100

    Exercise OOP_1

---
+++ OOP/_1/Course.java
@@ -0,0 +1,13 @@
+package Develhope_Exercise.OOP._1;
+
+import java.util.ArrayList;
+
+public class Course {
+    String courseName = null;
+    String maxStudents = null;
+    int qualityRatingOutOf10 = 0;
+    int maxRating = 0;
+    int minRating = 0;
+
+    ArrayList<Student> Students = new ArrayList();
+}
---
+++ OOP/_1/Student.java
@@ -0,0 +1,6 @@
+package Develhope_Exercise.OOP._1;
+
+public class Student {
+    String name = null;
+    int age = 0;
+}
